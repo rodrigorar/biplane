24f918004e7244ce8d661c79564ec882fc074a15
Rodrigo Rosa <rodrigo.rosa@premium-minds.com>
2020-04-18 17:27:12 +0100

    eviction: eviction task unit test

--- src/test/java/com/rodrigorar/biplane/eviction/TestEvictionTask.java
+++ src/test/java/com/rodrigorar/biplane/eviction/TestEvictionTask.java
@@ -1,21 +1,39 @@
 package com.rodrigorar.biplane.eviction;
 
+import com.rodrigorar.biplane.cache.InternalCache;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestEvictionTask {
 
 	@Test
 	public void testEvictionSuccess() {
-		// TODO: Not implemented
+		InternalCache mockedCache1 = Mockito.mock(InternalCache.class);
+		InternalCache mockedCache2 = Mockito.mock(InternalCache.class);
+		InternalCache mockedCache3 = Mockito.mock(InternalCache.class);
+
+		EvictionTask underTest = new EvictionTask();
+		underTest.addCache(mockedCache1);
+		underTest.addCache(mockedCache2);
+		underTest.addCache(mockedCache3);
+		underTest.run();
+
+		Mockito.verify(mockedCache1).evict();
+		Mockito.verify(mockedCache2).evict();
+		Mockito.verify(mockedCache3).evict();
+
+		Mockito.verifyNoMoreInteractions(mockedCache1, mockedCache2, mockedCache3);
 	}
 
 	@Test
 	public void testEvictionNoCaches() {
-		// TODO: Not implemented
+		EvictionTask underTest = new EvictionTask();
+		underTest.run();
 	}
 
 	@Test (expected = IllegalArgumentException.class)
 	public void testAddNullCache() {
-		// TODO: Not implemented
+		EvictionTask underTest = new EvictionTask();
+		underTest.addCache(null);
 	}
 }
